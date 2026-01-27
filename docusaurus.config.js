ad10ab317edde558a0a78099535525d0f4dcd566
PHRaulino <54751775+PHRaulino@users.noreply.github.com>
2026-01-27 00:50:59 -0300

    Atualizar o docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   url: 'https://phraulino-space.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub Pages deployment, it is often '/<projectName>/'
-  baseUrl: '/fintrack-docs/',
+  baseUrl: '/phrs-fintrack-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
